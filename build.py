d7f125df5da2dfc156cef0d6a73d37ecb00dc6ba
Dan Weatherill <plasteredparrot@gmail.com>
2019-01-13 07:34:36 +0000

    another try at proper conan config on travis CI

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    config_url = "https://github.com/bincrafters/conan-config.git"
+    
+    builder = ConanMultiPackager(config_url=config_url)
     builder.add_common_builds(pure_c=False)
     builder.run()
