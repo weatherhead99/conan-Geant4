d7879bf8373365461697a800550cc57678985236
Dan Weatherill <plasteredparrot@gmail.com>
2019-01-13 07:27:54 +0000

    add bincrafters remote

--- .travis/install.sh
+++ .travis/install.sh
@@ -23,3 +23,4 @@ pip install conan --upgrade
 pip install conan_package_tools bincrafters_package_tools
 
 conan user
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
